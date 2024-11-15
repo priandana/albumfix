9591bd022f2afa0f9bac0141679ff7f7b8ee12b9
priandana <40917449+priandana@users.noreply.github.com>
2024-11-15 14:25:24 +0700

    Update index.jsx

--- src/components/section/detail-info/index.jsx
+++ src/components/section/detail-info/index.jsx
@@ -11,9 +11,8 @@ export default function DetailInfo() {
   return (
     <div className="space-y-5 pb-10">
       <video className="w-full" autoPlay muted>
-        <source
-          src="https://youtu.be/Orv-DludwYM?si=jzK1t8Nwd4NIyKr7"
-        />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Orv-DludwYM?si=jzK1t8Nwd4NIyKr7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
         Your browser does not support the video tag.
       </video>
       <div className="px-4">
